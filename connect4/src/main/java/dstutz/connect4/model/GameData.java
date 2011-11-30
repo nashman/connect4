bfb970e38081884cd2ab0966fb83538f75c7efd0
Daniel Stutz <stutzda1@students.zhaw.ch>
2011-11-30 08:42:15 +0100

    Neues

---
+++ connect4/src/main/java/dstutz/connect4/model/GameData.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package dstutz.connect4.model;
+
+/**
+ * @author Daniel Stutz
+ * 
+ */
+public class GameData {
+	private int playMoveFromUser;
+	private int playMoveFromEngine;
+	private String[] setupData;
+
+	public String[] getSetupData() {
+		return setupData;
+	}
+
+	public void setSetupData(String[] setupData) {
+		// inital value for new game
+		setupData[2] = "0";
+		setupData[3] = "0";
+		this.setupData = setupData;
+	}
+}
